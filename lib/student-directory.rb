0850ab94dfbc692103a8927a09d041a03292a7a1
Painted <will.allen@paintedltd.co.uk>
2014-05-21 12:02:07 +0100

    added add student function

--- lib/student-directory.rb
+++ lib/student-directory.rb
@@ -1,15 +1,42 @@
 require 'csv'
 
 def student_list
-	@students = []
+	@students ||= []
 end
 
 def create_csv(filename)
-	CSV.open(filename, 'w')
+	CSV.open(filename, 'a')
 end
 
 def load_csv(filename)
 	CSV.foreach(filename) do |row|
-		row
+		@students = row
 	end
 end
+
+def add_student(name, cohort, email)
+	add_student_name(name)
+	add_student_cohort(cohort)
+	add_student_email(email)
+end
+
+def add_student_name(name)
+	student_list << {name: name}
+end
+
+def add_student_cohort(cohort)
+	student_list << {cohort: cohort}
+end
+
+def add_student_email(email)
+	student_list << {email: email}
+end
+
+def check_email?
+	@students[0][:email].include? "@"
+end
+
+def add_student_id(rg)
+	student_list << {rg: rg}
+end
+
--- spec/student-directory_spec.rb
+++ spec/student-directory_spec.rb
@@ -13,7 +13,42 @@ describe 'load data'  do
 
 	it 'reads data from the .csv file' do
 		expect(CSV).to receive(:foreach).with("directory.csv")
-		load_csv(filename)
+		load_csv("directory.csv")
+	end
+
+	it 'knows when the file is not empty' do
+		load_csv("directory.csv")
+		expect(student_list[0]).not_to be_empty
+	end
+
+
+end
+
+describe 'add student' do
+
+	it 'adds a student by name George' do
+		add_student_name('George')
+		expect(@students[0][:name]).to eq 'George'
+	end
+
+	it 'adds value to student for cohort May' do
+		add_student_cohort(:May)
+		expect(@students[0][:cohort]).to eq :May
+	end
+
+	it 'adds value to student for email' do
+		add_student_email('fsggfgs@dsff.com') 
+		expect(@students[0][:email]).to eq 'fsggfgs@dsff.com'
+	end
+
+	it 'checks to make sure the email includes @' do
+		add_student_email('fsggfgs@dsff.com') 
+		expect(check_email?).to be_true
+	end
+
+	it  'adds value to student for student id with length of 10' do
+		add_student_id('1234567891')
+		expect(@students[0][:rg].length).to eq 10
 	end
 
 end
